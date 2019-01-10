6be45a011b84a2d063d7d4b469e735f32803a949
zobad16 <zobad.mahmood@gmail.com>
2019-01-11 00:57:31 +0300

    Razor Changes

---
+++ Razor/Razor/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        Product myProduct = new Product
+        {
+            ProductID = 1,
+            Name = "Kayak",
+            Description = "A boat for one person",
+            Category = "Water Sports",
+            Price = 425M
+
+        };
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(myProduct);
+        }
+    }
+}
