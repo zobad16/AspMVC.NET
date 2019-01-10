31415108470b0274a0eb4c56372057d965c20292
zobad16 <zobad.mahmood@gmail.com>
2019-01-10 23:23:59 +0300

    First Commit

---
+++ Chapter2/PartyInvites2/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using PartyInvites2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Evening";
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid) {
+                //Todo: send email to the guest
+                return View("Thanks", guestResponse);
+            }
+            else {
+                return View();
+            }
+            
+        }
+    }
+}
---
+++ Chapter2/PartyInvites2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PartyInvites2.MvcApplication" Language="C#" %>
